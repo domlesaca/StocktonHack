c83f0a325c6c2d0493b7ef5a326805bd80a5419a
Jeff <j_wu35@u.pacific.edu>
2018-07-21 15:23:35 -0700

    swipe

--- Frontend/swipe.js
+++ Frontend/swipe.js
@@ -568,3 +568,14 @@ function Swipe(container, options) {
       }
     })( window.jQuery || window.Zepto )
   }
+
+  function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
