d1c374bfb227029b12b9d60e8ffe90fdb896e54c
domle <domlesaca@gmail.com>
2018-07-21 13:54:30 -0700

    convert form to JSON in register.html

---
+++ Frontend/registration.js
@@ -0,0 +1,42 @@
+
+function convertToJSON(formData) {
+    /*
+    let jsonObject = {}
+    formData.forEach(fucntion(IDBCursorWithValue, key){
+        jsonObject[key] = value;
+    });
+
+    return JSON.stringify(jsonObject);
+    */
+}
+
+(function() {
+	function toJSONString( form ) {
+		var obj = {};
+		var elements = form.querySelectorAll( "input, select, textarea" );
+		for( var i = 0; i < elements.length; ++i ) {
+			var element = elements[i];
+			var name = element.name;
+			var value = element.value;
+
+			if( name ) {
+				obj[ name ] = value;
+			}
+		}
+
+		return JSON.stringify( obj );
+	}
+
+	document.addEventListener( "DOMContentLoaded", function() {
+		var form = document.getElementById( "register-form" );
+		var output = document.getElementById( "output" );
+		form.addEventListener( "submit", function( e ) {
+			e.preventDefault();
+			var json = toJSONString( this );
+			output.innerHTML = json;
+
+		}, false);
+
+	});
+
+})();
